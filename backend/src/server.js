eac5fc27a6c02f00c64142b86e696173334f2d3d
Yesha Kushwaha <126480441+yeshakushwaha@users.noreply.github.com>
2025-10-27 23:34:02 +0530

    engines field added

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
