1bd88fa8fef135b5e5b8a48f0eab99fa27938ef8
Yesha Kushwaha <126480441+yeshakushwaha@users.noreply.github.com>
2025-10-28 12:04:09 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,9 +18,9 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (_,res) => {
+    app.get("*", (_, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+    });
 }
 
 
