23eb49bbe16f68ff2f9cdf1b333ca9ce9bdada0d
Yesha Kushwaha <126480441+yeshakushwaha@users.noreply.github.com>
2025-10-31 23:36:24 +0530

    added a new branch signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,48 +3,52 @@ import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-   const {fullname, email, password } = req.body
+  const { fullName, email, password } = req.body;
 
-   try {
-    if(!fullname || !email || !password){
-       return res.status(400).json({message:"All fields are required"});
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
     }
-    if(password.length < 6) {
-       return res.status(400).json({message:"Password must be atleast 6 characters"});
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
     }
-    // check if emails valid: regex
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^s@]+$/;
+
+    // check if emailis valid: regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-        return res.status(400).json({message:"Invalid email format"});
+      return res.status(400).json({ message: "Invalid email format" });
     }
-        const user = await User.findOne({email});
-            if(user) return res.status(400).json({message:"Email already exists"});
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "Email already exists" });
 
-            const salt = await bcrypt.genSalt(10)
-            const hashedPassword = await bcrypt.hash(password, salt)
+    // 123456 => $dnjasdkasj_?dmsakmk
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-            const newUser = new User({
-                fullname,
-                email,
-                password: hashedPassword
-            })
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
 
-            if(newUser) {
-                generateToken(newUser._id, res)
-                await newUser.save()
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
 
-                res.status(201).json({
-                    _id:newUser._id,
-                    fullName:newUser.fullName,
-                    email:newUser.email,
-                    profilePic:newUser.profilePic,
-                })
-            } else{
-                res.status(400).json({message: "Invalid user data"});
-            }
-
-   } catch (error) {
-        console.log("Error in signup controller:", error);
-        res.status(500).json({message: "Internal server error"});
-   }
-}
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,28 +1,29 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     fullName: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 6,
+      type: String,
+      required: true,
+      minlength: 6,
     },
     profilePic: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
-}, { timestamps: true } // createdAt & updatedAt
-
+  },
+  { timestamps: true } // createdAt & updatedAt
 );
 
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 
 export default User;
